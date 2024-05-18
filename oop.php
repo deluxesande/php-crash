edd1d7c253d7d2fb1f2db8f019fc9372c96f20e4
deluxesande <deluxesande@gmail.com>
2024-05-18 19:19:48 +0300

    (Add): basic OOP complete

--- oop.php
+++ oop.php
@@ -4,16 +4,33 @@
         public $email;
         public $password;
 
+        // Constructor
+        public function __construct($name, $email, $password) {
+            $this->name = $name;
+            $this->email = $email;
+            $this->password = $password;
+        }
+
         // Method
+        // Setters
         function set_name($name) {
             $this->name = $name;
         }
+
+        function get_name() {
+            return $this->name;
+        }
+
+        function get_email() {
+            return $this->email;
+        }
     }
 
-    $user1 = new User();
-
-    $user1->set_name('John Doe');
+    $user1 = new User("John Doe", "johndoe@gmail.com", "123456");
 
     // var_dump($user1);
-    echo $user1->name;
+    echo $user1->get_email();
+
+
+    // Inheritance
 ?>
